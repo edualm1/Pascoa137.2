8c32415056131ef2d21435bc0f4f9b3ac9f44a29
pc-sala <edualm1@hotmail.com>
2023-03-31 16:11:00 -0300

    Páscoa 137.2 Eduardo

--- src/test/java/apiTest/TesteUser.java
+++ src/test/java/apiTest/TesteUser.java
@@ -12,10 +12,13 @@ import java.nio.file.Paths;
 import static io.restassured.RestAssured.given;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.matchesPattern;
 
 // Classe
 public class TesteUser { // Início da classe
     // Atributos
+    String ct = "application/json"; // content type
+    String uriUser = "https://petstore.swagger.io/v2/user";
 
     // Funções e métodos
 
@@ -34,19 +37,45 @@ public class TesteUser { // Início da classe
 
         // realizar o teste
         given()                                         // Dado que
-                .contentType("application/json")     // o tipo do conteúdo
+                .contentType(ct)                        // o tipo do conteúdo
                 .log().all()                            // mostre tudo
                 .body(jsonBody)                         // corpo da requisição
 
         .when()
-                .post("https://petstore.swagger.io/v2/user") // Endpoint / Onde
+                .post(uriUser) // Endpoint / Onde
         .then()                                          // Então
                 .log().all()                            // mostre tudo na volta
                 .statusCode(200)                      // comunic. ida e volta ok
                 .body("code", is(200))          // tag code é 200
-                .body("type", is("unknwon"))    // tag type é "unknown"
+                .body("type", is("unknown"))    // tag type é "unknown"
                 .body("message", is(userId))         // message é o userId
         ;
+    }       // fim do post
+
+    @Test
+    public void testarConsultarUser()  throws IOException {
+        String username = "du";
+
+        // resultado esperado
+        int userId = 1373709898;       // codigo do usuario
+        String email = "almeida@test.com";
+        String senha = "123456";
+        String telefone = "11912345678";
+
+    given()
+            .contentType(ct)
+            .log().all()
+    .when()
+            .get(uriUser + username)      // onde
+    .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is("du"))
+                .body("email", is("almeida@test.com"))
+                .body("phone", is("telefone"))
+                .body("senha", is("123456"))
+    ;
     }
 
 } // fim da classe
+
